c4a1c106600657f8bbd00ecaefcdc2ed0e5e0aa8
LordSequoia <thelordsequoia@gmail.com>
2022-07-16 13:57:11 +0000

    wip

--- src/lib/workspace.ts
+++ src/lib/workspace.ts
@@ -1,3 +1,4 @@
+import { existsSync, mkdirSync } from 'node:fs'
 import { homedir } from 'node:os'
 import { join, resolve } from 'node:path'
 
@@ -6,14 +7,22 @@ import { createRandomName } from './helpers'
 export type Workspace = {
     readonly rootDir: string
     readonly name: string
+    readonly existed: boolean
 }
 
 export const createWorkspace = (name?: string, parentDir?: string): Workspace => {
     const workspaceName = name || createRandomName()
     const workspaceDir = join(parentDir || resolve(homedir(), '.workspaces'), workspaceName)
 
+    const existed = existsSync(workspaceDir)
+
+    if (!existed) {
+        mkdirSync(workspaceDir, { recursive: true })
+    }
+
     return {
         name: workspaceName,
         rootDir: workspaceDir,
+        existed,
     }
 }
